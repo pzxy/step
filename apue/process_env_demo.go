578fdda1e27ae02c28959f58f45e09d1cb536962
pzxy <wonkung@163.com>
2020-07-19 21:42:03 +0800

    env

---
+++ apue/process_env_demo.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"step/utils/log"
+)
+
+func main() {
+	changeEnvNameDemo()
+}
+
+/**
+
+todo
+
+关于syscall和os包的思考，是不是syscall包底层是内核，而os包是用户呀？这里不懂，疑惑。
+*/
+
+/**
+env
+*/
+func envDemo() {
+	//命令行设置 直接 WWW = www.socrates.com 就行
+	fmt.Printf("get env:%v \n", os.Getenv("WWW")) //区分大小写
+	err := os.Setenv("WWW", "www.meetsocrates.com")
+	if err != nil {
+		log.ErrLog(err)
+		return
+	}
+	fmt.Printf("after set env  :%v \n", os.Getenv("WWW")) //区分大小写
+
+	err = os.Unsetenv("WWW")
+	if err != nil {
+		log.ErrLog(err)
+		return
+	}
+	fmt.Printf("after delete env  :%v \n", os.Getenv("WWW")) //区分大小写
+}
+
+func changeEnvNameDemo() {
+	oldEnv := "WWW"
+	newEnv := "W4"
+	err := os.Setenv(oldEnv, "www.meetsocrates.com")
+	if err != nil {
+		log.ErrLog(err)
+		return
+	}
+	envValue := os.Getenv(oldEnv)
+	if len(envValue) == 0 {
+		fmt.Printf("env `WWW` does not exist \n")
+		return
+	}
+	fmt.Printf("before env WWW change :%v \n", envValue) //区分大小写
+
+	err = os.Unsetenv(oldEnv)
+	if err != nil {
+		log.ErrLog(err)
+		return
+	}
+	err = os.Setenv(newEnv, envValue)
+	if err != nil {
+		log.ErrLog(err)
+		return
+	}
+	fmt.Printf("after env WWW change :%v \n", os.Getenv(newEnv)) //区分大小写
+
+	err = os.Unsetenv(newEnv)
+	if err != nil {
+		log.ErrLog(err)
+		return
+	}
+	fmt.Printf("after env WWW delete :%v \n", os.Getenv(newEnv)) //区分大小写
+
+}
