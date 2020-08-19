1f3564cd8f699daf3153999655c604945e112083
pzxy <wonkung@163.com>
2020-08-19 21:27:50 +0800

    lowest common ancestor

--- arithmetic/leetcode/demo_test.go
+++ arithmetic/leetcode/demo_test.go
@@ -83,3 +83,29 @@ func Test_largestNumber(t *testing.T) {
 	s3 := []int{1, 2, 3, 1}
 	fmt.Println(largestNumber(s3))
 }
+
+func Test_lowestCommonAncestor(t *testing.T) {
+	//root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1 结果3
+	// p = 5, q = 4,结果5
+	/**
+	  如果节点 X 存储在数组中下标为 i 的位置，下标为 2 * i 的位置存储的就是左子节点，
+	  	下标为 2 * i + 1 的位置存储的就是右子节点。反过来，下标为 i/2 的位置存储就是它的父节点。
+	  	通过这种方式，我们只要知道根节点存储的位置（一般情况下，为了方便计算子节点，根节点会存储在下标为 1 的位置），
+	  	这样就可以通过下标计算，把整棵树都串起来。
+	*/
+	n6 := &TreeNode{Val: 6}
+	n0 := &TreeNode{Val: 0}
+	n8 := &TreeNode{Val: 8}
+	n7 := &TreeNode{Val: 7}
+	n4 := &TreeNode{Val: 4}
+
+	n2 := &TreeNode{Val: 2, Left: n7, Right: n4}
+	n5 := &TreeNode{Val: 5, Left: n6, Right: n2}
+	n1 := &TreeNode{Val: 1, Left: n0, Right: n8}
+	n3 := &TreeNode{Val: 3, Left: n5, Right: n1}
+
+	fmt.Println(lowestCommonAncestor2(n3, n5, n1).Val)
+
+	fmt.Println(lowestCommonAncestor2(n3, n5, n4).Val)
+
+}
