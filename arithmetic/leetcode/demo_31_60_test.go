4880bea5766ecf39e9123cc5990386c36ca9ec66
pzxy <wonkung@163.com>
2020-09-15 21:50:03 +0800

    level Order i ii iii

--- arithmetic/leetcode/demo_31_60_test.go
+++ arithmetic/leetcode/demo_31_60_test.go
@@ -48,3 +48,75 @@ func Test_levelOrder(t *testing.T) {
 		}
 	}
 }
+
+func Test_levelOrder2(t *testing.T) {
+	/**
+	    3
+	   / \
+	  9  20
+	/  \  /  \
+	1   2  15   7
+	返回：
+	[[3],[9,20],[1,2,15,7]]
+	*/
+	root := &TreeNode{Val: 3}
+	root.Left = &TreeNode{Val: 9}
+	root.Left.Left = &TreeNode{Val: 1}
+	root.Left.Right = &TreeNode{Val: 2}
+	root.Right = &TreeNode{Val: 20}
+	root.Right.Left = &TreeNode{Val: 15}
+	root.Right.Right = &TreeNode{Val: 7}
+	target := [][]int{
+		{3},
+		{9, 20},
+		{1, 2, 15, 7},
+	}
+	ret := levelOrder2(root)
+	if ret == nil {
+		log.ErrLog(errors.New("offer32 level order fail"))
+	}
+
+	for i, v := range target {
+		for j, vv := range v {
+			if ret[i][j] != vv {
+				log.ErrLog(errors.New("offer32 level order fail"))
+			}
+		}
+	}
+}
+
+func Test_levelOrder3(t *testing.T) {
+	/**
+	    3
+	   / \
+	  9  20
+	/  \  /  \
+	1   2  15   7
+	返回：
+	[[3],[20,9],[1,2,15,7]]
+	*/
+	root := &TreeNode{Val: 3}
+	root.Left = &TreeNode{Val: 9}
+	root.Left.Left = &TreeNode{Val: 1}
+	root.Left.Right = &TreeNode{Val: 2}
+	root.Right = &TreeNode{Val: 20}
+	root.Right.Left = &TreeNode{Val: 15}
+	root.Right.Right = &TreeNode{Val: 7}
+	target := [][]int{
+		{3},
+		{20, 9},
+		{1, 2, 15, 7},
+	}
+	ret := levelOrder3(root)
+	if ret == nil {
+		log.ErrLog(errors.New("offer32 level order fail"))
+	}
+
+	for i, v := range target {
+		for j, vv := range v {
+			if ret[i][j] != vv {
+				log.ErrLog(errors.New("offer32 level order fail"))
+			}
+		}
+	}
+}
