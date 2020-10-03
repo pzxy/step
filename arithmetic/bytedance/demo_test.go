26b0648f802ab068356bf618b6ea475386359010
pzxy <wonkung@163.com>
2020-10-03 22:31:25 +0800

    rob

--- arithmetic/bytedance/demo_test.go
+++ arithmetic/bytedance/demo_test.go
@@ -2,6 +2,7 @@ package bytedance
 
 import (
 	"errors"
+	"fmt"
 	"step/grammar/codeskill/log"
 	"testing"
 )
@@ -26,5 +27,59 @@ func TestDemo1P2(t *testing.T) {
 			break
 		}
 		l3 = l3.Next
+	}
+}
+
+func Test_minimumOperations(t *testing.T) {
+	/**
+	输入：leaves = "rrryyyrryyyrr"
+
+	输出：2
+
+	解释：调整两次，将中间的两片红叶替换成黄叶，得到 "rrryyyyyyyyrr"
+	示例 2：
+
+	输入：leaves = "ryr"
+	*/
+	strs := []string{
+		"rrryyyrryyyrr", "ryr", "rrrrr", "yyyyy",
+	}
+	target := []int{2, 0, 1, 2}
+	for i, s := range strs {
+		ret := minimumOperations(s)
+		if ret != target[i] {
+			log.ErrLog(errors.New(fmt.Sprintf("minimumOperations fail:s:%s,target:%d,ret:%d \n\t", s, target[i], ret)))
+		}
+	}
+}
+
+func Test_scoreOfParentheses(t *testing.T) {
+	/**
+	输入： "()"
+	输出： 1
+	示例 2：
+
+	输入： "(())"
+	输出： 2
+	示例 3：
+
+	输入： "()()"
+	输出： 2
+	示例 4：
+
+	输入： "(()(()))"
+	输出： 6
+	*/
+	strs := []string{
+		"()", "(())", "()()", "(()(()))",
+	}
+	target := []int{
+		1, 2, 2, 6,
+	}
+	for i, s := range strs {
+		ret := scoreOfParentheses(s)
+		if ret != target[i] {
+			log.ErrLog(errors.New(fmt.Sprintf("scoreOfParentheses fail:s:%s,target:%d,ret:%d \n\t", s, target[i], ret)))
+		}
 	}
 }
