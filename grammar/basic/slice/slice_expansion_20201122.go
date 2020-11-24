b4adcb188807723a788e062709cd73fee29893a9
pzxy <wonkung@163.com>
2020-11-24 20:00:05 +0800

    select

--- grammar/basic/slice/slice_expansion_20201122.go
+++ grammar/basic/slice/slice_expansion_20201122.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	s1 := make([]int, 5, 10)
@@ -9,4 +12,12 @@ func main() {
 	fmt.Printf("s1 :len:%v,cap:%v,%p,%v \n", len(s1), cap(s1), s1, s1)
 	fmt.Printf("s2 :len:%v,cap:%v,%p,%v \n", len(s2), cap(s2), s2, s2)
 	fmt.Printf("s3 :len:%v,cap:%v,%p,%v \n", len(s3), len(s3), s3, s3)
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			fmt.Printf("1")
+			//break
+		}
+	}()
+	select {} //只要有其他协程没有退出，这里就不会有问题，否则会发生死锁。
 }
