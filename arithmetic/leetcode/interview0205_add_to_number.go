47444462e1833f3ffce2aaf49834e4818f377761
pzxy <wonkung@163.com>
2021-01-18 17:56:43 +0800

    addTwoNumbers for list

---
+++ arithmetic/leetcode/interview0205_add_to_number.go
@@ -0,0 +1,50 @@
+package leetcode
+
+/**
+给定两个用链表表示的整数，每个节点包含一个数位。
+
+这些数位是反向存放的，也就是个位排在链表首部。
+
+编写函数对这两个整数求和，并用链表形式返回结果。
+
+示例：
+
+输入：(7 -> 1 -> 6) + (5 -> 9 -> 2)，即617 + 295
+输出：2 -> 1 -> 9，即912
+进阶：思考一下，假设这些数位是正向存放的，又该如何解决呢?
+
+示例：
+
+输入：(6 -> 1 -> 7) + (2 -> 9 -> 5)，即617 + 295
+输出：9 -> 1 -> 2，即912
+
+*/
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	head := &ListNode{-1, nil}
+	currNode := head
+	var carry int
+
+	for l1 != nil || l2 != nil || carry > 0 {
+		var v int
+		if l1 != nil {
+			v += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			v += l2.Val
+			l2 = l2.Next
+		}
+		v += carry
+		carry = v / 10
+		newNode := &ListNode{Val: v % 10}
+		currNode.Next = newNode
+		currNode = newNode
+	}
+	return head.Next
+}
