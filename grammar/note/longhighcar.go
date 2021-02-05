1789e6dcc88dd6c78d9fbbd36cb531a3c6a1a786
pzxy <wonkung@163.com>
2021-02-05 10:22:53 +0800

    array 2 tree

--- grammar/note/longhighcar.go
+++ grammar/note/longhighcar.go
@@ -5,15 +5,11 @@ import (
 )
 
 func main() {
-	//a := 0
-	//fmt.Scan(&a)
-	//fmt.Printf("%d\n", a)
-
-	testData := []int{0, 1, 2}
+	testData := []int{6, 4, 8, 0, 0, 7, 9}
 	res := tranTree2(testData)
 
 	result := inOrder(res)
-	fmt.Printf("result =%v", res)
+	fmt.Printf("result =%v", result)
 }
 
 type TreeNode1 struct {
@@ -23,19 +19,32 @@ type TreeNode1 struct {
 }
 
 func tranTree2(data1 []int) *TreeNode1 {
-	rootIdx := 0
-	root := &TreeNode1{}
-	inOrder4TranTree(root, data1, rootIdx)
+	head := &TreeNode1{}
+	root := head
+	for i := 0; i <= len(data1)/2; i++ {
+		if data1[i] == 0 {
+			continue
+		}
+		root.Val = data1[i]
+		if data1[2*i+1] != 0 {
+			root.Left = &TreeNode1{Val: data1[2*i+1]}
+		}
+		if data1[2*i+2] != 0 {
+			root.Right = &TreeNode1{Val: data1[2*i+2]}
+		}
+	}
 	return root
 }
 
 func inOrder4TranTree(root *TreeNode1, data []int, rootIdx int) {
-	if rootIdx > len(data)/2 {
+	if rootIdx >= len(data) {
 		return
 	}
+	root.Left = &TreeNode1{}
+	inOrder4TranTree(root.Left, data, rootIdx*2+1)
 	root.Val = data[rootIdx]
-	root.Left = &TreeNode1{Val: data[rootIdx*2+1]}
-	root.Right = &TreeNode1{Val: data[rootIdx*2+2]}
+	root.Right = &TreeNode1{}
+	inOrder4TranTree(root.Right, data, rootIdx*2+2)
 }
 
 func TranArray(node1 *TreeNode1) (ret []int) {
@@ -43,9 +52,33 @@ func TranArray(node1 *TreeNode1) (ret []int) {
 		return
 	}
 	Q := []*TreeNode1{node1}
-	for len(Q) > 0 {
-		r := Q[0]
-		ret = append()
+	var nonNilP = true //队列存在非空节点？继续:结束；
+	for len(Q) > 0 && nonNilP {
+		tmp := Q
+		Q = make([]*TreeNode1, 0)
+		nonNilP = false
+		for len(tmp) > 0 {
+			q := tmp[0]
+			tmp = tmp[1:]
+			if q == nil {
+				ret = append(ret, 0)
+				continue
+			}
+			ret = append(ret, q.Val)
+
+			if q.Left == nil {
+				Q = append(Q, nil)
+			} else {
+				nonNilP = true
+				Q = append(Q, q.Left)
+			}
+			if q.Right == nil {
+				Q = append(Q, nil)
+			} else {
+				nonNilP = true
+				Q = append(Q, q.Right)
+			}
+		}
 	}
 	return
 }
