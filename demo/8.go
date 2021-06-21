e7f35a5d18f7d9ef2687198fe14463268339500a
王坤 <tielin.wang@tuya.com>
2021-06-21 09:44:35 +0800

    mac

--- demo/8.go
+++ demo/8.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/hex"
 	"fmt"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -10,10 +13,48 @@ func main() {
 	if err != nil {
 		return
 	}
+
 	var mac string
+	var hhhhex [][]byte
+	s := "14:7d:da:e:f3:0"
+
+	for _, v := range strings.Split(s, ":") {
+		temp := []rune(v)
+		if len(temp) == 0 {
+			v = "00"
+		}
+		if len(temp) == 1 {
+			v = "0" + v
+		}
+		b, err := hex.DecodeString(v)
+		if err != nil {
+			fmt.Errorf("err:%s", err)
+		}
+		hhhhex = append(hhhhex, b)
+	}
+
 	for _, inter := range interfaces {
+		inter.HardwareAddr.String()
 		mac = inter.HardwareAddr.String()
-		fmt.Println(mac == "14:7d:da:0e:f3:00")
+		s := strings.Split(mac, ":")
+		if len(s) != len(hhhhex) {
+			fmt.Println("error：", mac)
+			continue
+		}
+		var f bool
+		for i, v := range s {
+			b, _ := hex.DecodeString(v)
+			if bytes.Compare(b, hhhhex[i]) != 0 {
+				fmt.Println("不相等")
+				f = true
+				break
+			}
+		}
+		if f {
+			continue
+		}
+
+		fmt.Println(mac)
 	}
 	//	origData, err = aes_crypto.GcmDecrypt(crypted, encryptKey(mac))
 	//	if err != nil {
