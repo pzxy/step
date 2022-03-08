181c864720a525da103b1a96f16c05857900d19a
王坤 <tielin.wang@tuya.com>
2022-03-08 11:09:31 +0800

    clean

--- db/leveldb/myleveldb/time_test.go
+++ db/leveldb/myleveldb/time_test.go
@@ -2,9 +2,9 @@ package myleveldb
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
-	"github.com/syndtr/goleveldb/leveldb"
-	"log"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -24,26 +24,32 @@ func TestTime(t *testing.T) {
 
 //TestRegularClean 测试清理规则
 func TestRegularClean(t *testing.T) {
-	c, err := NewClient("./db/leveldb/data/")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	go c.regularClean()
-	//设置5秒，睡10秒以后 再获取，应该已经被清理过了
-	err = c.PutStringExpire("a", "v", time.Second*10)
-	if err != nil {
-		log.Println("err:", err)
-		return
-	}
-	time.Sleep(time.Second * 100)
-	v, err := c.getValue("a")
-	if err != leveldb.ErrNotFound {
-		log.Fatalln("TestRegularClean fail ")
-	}
-	fmt.Println(v)
+	//c, err := NewClient("./db/leveldb/data/")
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+	//go c.regularClean()
+	////设置5秒，睡10秒以后 再获取，应该已经被清理过了
+	//err = c.PutStringExpire("a", "v", time.Second*10)
+	//if err != nil {
+	//	log.Println("err:", err)
+	//	return
+	//}
+	//time.Sleep(time.Second * 100)
+	//v, err := c.getValue("a")
+	//if err != leveldb.ErrNotFound {
+	//	log.Fatalln("TestRegularClean fail ")
+	//}
+	//fmt.Println(v)
 	//Convey("TestRegularClean", t, func() {
 	//	Convey("a", func() {
 	//		So(leveldb.ErrNotFound, ShouldEqual, err.Error())
 	//	})
 	//})
+	src := []byte{102, 55, 72, 51, 50, 108, 98, 89, 74, 88, 117, 49, 82, 89, 55, 104}
+	toString := hex.EncodeToString(src)
+	fmt.Println(toString)
+	fmt.Println(strconv.FormatInt(time.Now().UnixNano(), 10))
+	fmt.Println(time.Now().UnixNano())
+	fmt.Println(time.Now().Unix() + 2592000)
 }
