d0ed82a296bcc9a7ab130e2053f87ed9208372d5
王坤 <tielin.wang@tuya.com>
2022-07-27 17:15:39 +0800

    doc

--- demo1/35_endian.go
+++ demo1/35_endian.go
@@ -2,18 +2,40 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
+	"io"
+	"strings"
 )
 
+type AAAA struct {
+	B string `json:"b"`
+}
+
 func main() {
-	fmt.Println(IntToBytes(123))
-	i := uint32(1)
-	u := i - 2
-	fmt.Println(u)
-	for i := 0; i < 102; i++ {
-		fmt.Println(i & 15)
+	var body io.Reader
+	if body == nil {
+		fmt.Println("asd")
 	}
+	str := "eyJodW1pZGl0eSI6MTAwLCJ0ZW1wIjoyMDAsInBoVmFsdWUiOjAuMCwiZWNWYWx1ZSI6MC4wfQ=="
+
+	b64 := base64.NewDecoder(base64.StdEncoding, strings.NewReader(str))
+	bs := make([]byte, len(str))
+	if n, err := b64.Read(bs); err != nil {
+		panic(err)
+	} else {
+		bs = bs[:n]
+	}
+	fmt.Println(string(bs))
+
+	base64.NewDecoder(base64.StdEncoding, strings.NewReader(str))
+	bb := `{"b":"eyJodW1pZGl0eSI6MTAwLCJ0ZW1wIjoyMDAsInBoVmFsdWUiOjAuMCwiZWNWYWx1ZSI6MC4wfQ=="}`
+	var a AAAA
+	json.Unmarshal([]byte(bb), &a)
+	fmt.Println(a)
+
 }
 
 func IntToBytes(n int) []byte {
--- grammar/gotools/goconvey/demo_test.go
+++ grammar/gotools/goconvey/demo_test.go
@@ -1,11 +1,12 @@
 package goconvey
 
 import (
-	"bou.ke/monkey"
 	"fmt"
-	. "github.com/smartystreets/goconvey/convey"
 	"reflect"
 	"testing"
+
+	"bou.ke/monkey"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestDemo1(t *testing.T) {
@@ -24,32 +25,32 @@ func TestDemo1(t *testing.T) {
 	})
 }
 
-//打桩 go get github.com/prashantv/gostub
+// 打桩 go get github.com/prashantv/gostub
 
-//使用打桩方法测试
+// 使用打桩方法测试
 func TestDemo2(t *testing.T) {
 	md := &MathDemo{}
-	//结构体的方法打桩
+	// 结构体的方法打桩
 	monkey.PatchInstanceMethod(reflect.TypeOf(md), "Sub", SubStub)
-	//简单方法打桩
-	//monkey.Patch(Sum, SumStub)
+	// 简单方法打桩
+	monkey.Patch(Sum, SumStub)
 
-	//Convey("TestSumStub", t, func() {
+	// Convey("TestSumStub", t, func() {
 	//	Convey("TestSumStub", func() {
 	// 场景：地图中找不到该点
 
 	m := &MathDemo{10, 2}
 
 	fmt.Println(m.Sub())
-	//p := result == 6
-	//So(p, ShouldBeTrue)
-	//})
-	//Convey("TestSubStub", func() {
+	// p := result == 6
+	// So(p, ShouldBeTrue)
+	// })
+	// Convey("TestSubStub", func() {
 	//	// 场景：地图中找不到该点
 	//	m := &MathDemo{10, 2}
 	//	result := SubStub(m)
 	//	p := result == 5
 	//	So(p, ShouldBeTrue)
-	//})
+	// })
 
 }
