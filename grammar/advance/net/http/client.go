a77d76c3ee7985c4f6ec838eb69747974308d7d4
王坤 <tielin.wang@tuya.com>
2022-08-30 09:47:55 +0800

    pow

--- grammar/advance/net/http/client.go
+++ grammar/advance/net/http/client.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
 )
 
 func main() {
-	request, err := http.NewRequest(http.MethodGet, "http://www.imoooc.com", nil) //访问此url 通过header跳转
+	request, err := http.NewRequest(http.MethodGet, "http://www.imoooc.com", nil) // 访问此url 通过header跳转
 	request.Header.Add("User-Agent", " Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1")
-	client := http.Client{ //自定义一个client
+	client := http.Client{ // 自定义一个client
 		CheckRedirect: func(
 			req *http.Request,
 			via []*http.Request) error {
@@ -17,14 +18,15 @@ func main() {
 			return nil
 		},
 	}
+	zip.NewWriter()
 	resp, err := client.Do(request)
-	//resp, err := http.DefaultClient.Do(request)
+	// resp, err := http.DefaultClient.Do(request)
 	if err != nil {
 		panic(err)
 	}
 
 	defer resp.Body.Close()
-	//binary.Write()
+	// binary.Write()
 	s, err := httputil.DumpResponse(resp, true)
 	if err != nil {
 		panic(err)
