6ff726e4e78858ee142d5fbd6d0d431c6b1b6a0a
王坤 <tielin.wang@tuya.com>
2022-09-09 16:26:54 +0800

    readme.md

--- share/test-demo/fuzz_test.go
+++ share/test-demo/fuzz_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"testing"
-	"unicode/utf8"
 )
 
 func TestReverse(t *testing.T) {
@@ -22,15 +21,15 @@ func TestReverse(t *testing.T) {
 }
 
 // FuzzReverse go test -fuzz=Fuzz
-func FuzzReverse(f *testing.F) {
-	f.Fuzz(func(t *testing.T, orig string) {
-		rev := Reverse(orig)
-		doubleRev := Reverse(rev)
-		if orig != doubleRev {
-			t.Errorf("Before: %q, after: %q", orig, doubleRev)
-		}
-		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
-			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
-		}
-	})
-}
+// func FuzzReverse(f *testing.F) {
+// 	f.Fuzz(func(t *testing.T, orig string) {
+// 		rev := Reverse(orig)
+// 		doubleRev := Reverse(rev)
+// 		if orig != doubleRev {
+// 			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+// 		}
+// 		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+// 			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+// 		}
+// 	})
+// }
