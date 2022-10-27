c94042a68f207c64c82062104faee8554689705b
pzxy <wonkung@163.com>
2022-10-27 14:53:55 +0800

    udp

--- cgo/pcre2/pcre2_test.go
+++ cgo/pcre2/pcre2_test.go
@@ -1,11 +1,12 @@
 package pcre2
 
 import (
+	"fmt"
+	"net"
 	"testing"
 )
 
 func TestMatch(t *testing.T) {
-
 	data := []struct {
 		pattern string
 		subject string
@@ -44,4 +45,38 @@ func TestMatch(t *testing.T) {
 			}
 		}
 	}
+}
+
+func TestMatchAndSendMsg2US(t *testing.T) {
+	pattern := `\d{4}[^0-9^\s]{3,11}\S`
+	subject := `a;jhgoqoghqoj0329 u0tyu10hg0h9Y0Y9827342482y(Y0y(G)_)lajf;lqjfgqhgpqjopjqa=)*(^!@#$%^&*())9999999`
+	ret, err := Match(pattern, subject)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	for _, v := range ret {
+		msg := v[4 : len(v)-1]
+		if err = sendMsg2UDPServer(msg); err != nil {
+			t.Error(err)
+			return
+		}
+	}
+}
+
+func sendMsg2UDPServer(msg string) error {
+	socket, err := net.DialUDP("udp", nil, &net.UDPAddr{
+		IP:   net.IPv4(0, 0, 0, 0),
+		Port: 3000,
+	})
+	if err != nil {
+		return fmt.Errorf("connect fail，err: %s", err)
+	}
+	defer socket.Close()
+	sendData := []byte(msg)
+	_, err = socket.Write(sendData) // 发送数据
+	if err != nil {
+		return fmt.Errorf("send message fail，err: %s", err)
+	}
+	return nil
 }
